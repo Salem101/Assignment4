41f85626e650c7c2ae06ab2c3a5479955765fdcb
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2020-08-11 21:55:06 -0400

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,3 +1,7 @@
+//List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+
+
 SELECT NAME, CONTINENT
 FROM world
 ORDER BY NAME
