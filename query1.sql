cb345867ae60b5dc53ad5175ccff4fb6fec588c7
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2020-08-11 23:18:31 -0400

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,6 +1,8 @@
 //List each country name where the population is larger than that of 'Russia'.
 
-
+//selecting name from the world table
+//taking population from world table and getting results that name are Romania
+//Whenever population is greater than that result we return name. 
 
 SELECT name FROM world
   WHERE population >
